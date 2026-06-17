91c6ffd3ae57717ee3e539b2a8d3542bdcf0e87e
SpazzSpud <tntnorton88@gmail.com>
2026-06-17 14:22:08 +0200

    lstm tuned results

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 
 LSTM_TUNABLE_PARAMS = {
-    "hidden_size": 64,
+    "hidden_size": 128,
     "num_layers": 1,
-    "dropout": 0.3,
-    "n_epochs": 30,
-    "lr": 1e-3,
-    "weight_decay": 1e-4,
+    "dropout": 0.484,
+    "n_epochs": 100,
+    "lr": 0.00293,
+    "weight_decay": 0.00722,
     "window": 200,
     "stride": 75,
     "decim": 2,
     "aug_noise": 0.05,
-    "aug_scale": 0.10,
+    "aug_scale": 0.116,
 }
 
 LSTM_SPACE = {
